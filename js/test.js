cb344ae696787302eb665f8e24b4e0388d0bc6b7
admin <llooll3170@gmail.com>
2024-03-27 17:35:35 +0900

    4주차 찐완

--- js/test.js
+++ js/test.js
@@ -2,3 +2,26 @@ var jb = 'hi';
 var a = 1;
 var b;
 b = 5;
+
+if (true) {
+  let c = 'let접근';
+  var c_1 = 'var접근';
+}
+//console.log(c);//Error?
+console.log(c_1);
+
+let d = 5;
+//let d='값 재할당';//Error?
+console.log(d);
+
+const e = '상수1접근';
+//e=5;
+//const f//Error?
+console.log(e);
+
+//마우스//
+document.getElementById('search_btn').addEventListener('click', search_message);
+
+function search_message() {
+  alert('검색을 수정합니다!');
+}
