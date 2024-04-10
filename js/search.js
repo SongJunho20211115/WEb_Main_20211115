96726f78a7c3659d432c8a4546a5aa0c5a6d61d1
admin <llooll3170@gmail.com>
2024-04-10 15:40:41 +0900

    최종 해벼럿짜나!!

--- js/search.js
+++ js/search.js
@@ -1,5 +1,15 @@
 document.getElementById('search_btn').addEventListener('click', search_message);
 
 function search_message() {
-  alert('존나게 검색합니다');
+  alert('검색합니다');
+}
+
+function googleSearch() {
+  // 검색어로 설정
+  const searchTerm = document.getElementById('search_input').value;
+  const googleSearchurl =
+    'https://www.google.com/search?q=${encodeURIComponent(searchTerm)}';
+  // 세 창에서 구글 검색을 수행
+  window.open(googleSearchurl, '_blank');
+  return false;
 }
